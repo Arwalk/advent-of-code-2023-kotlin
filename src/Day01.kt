45415b2168ece7f8e3680684537eecd5746f00d5
Arwalk <siragentprovocateurarwalk@gmail.com>
2023-12-01 20:34:53 +0100

    much more complicated functional solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,29 +1,67 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.map {
-            it.filter { c -> c.isDigit() }
-        }.map {
-            "${it.first()}${it.last()}"
-        }.sumOf {
-            it.toInt()
+
+    fun solver(finder : (String) -> (List<Int>)) : (input: List<String>) -> (Int) {
+        return { input: List<String> ->
+            input.map(finder).sumOf {
+                (it.first() * 10) + it.last()
+            }
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.map {
-            it.replace("one", "one1one")
-                .replace("two", "two2two")
-                .replace("three", "three3three")
-                .replace("four", "four4four")
-                .replace("five", "five5five")
-                .replace("six", "six6six")
-                .replace("seven", "seven7seven")
-                .replace("eight", "eight8eight")
-                .replace("nine", "nine9nine")
-        }.let {
-            part1(it)
+    val part1 = solver {
+        return@solver it.filter {
+            c -> c.isDigit()
+        }.map { c -> c.toString().toInt() }
+    }
+
+    fun findAllOccAndMap(str: String, v: Int) : (String) -> List<Pair<Int, Int>> {
+        return { it ->
+            val collected = mutableListOf<Pair<Int, Int>>()
+            var index = 0
+            while(index != -1) {
+                index = it.indexOf(str, index).let {i ->
+                    if(i != -1) {
+                        collected.add(i to v)
+                        i+1
+                    } else {
+                        i
+                    }
+
+                }
+            }
+            collected.toList()
         }
     }
+
+    val namesAndValues = listOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    val part2 = solver { str ->
+        val simpleDigits = str.mapIndexedNotNull {
+            i, c -> if(c.isDigit()) {
+                i to c.toString().toInt()
+            } else {
+                null
+            }
+        }
+        val complexes = namesAndValues.map {nv ->
+            findAllOccAndMap(nv.first, nv.second)(str)
+        }.flatten()
+
+        (simpleDigits + complexes)
+            .sortedBy { it.first }
+            .map { it.second }
+    }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
